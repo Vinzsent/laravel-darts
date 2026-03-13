aaffad92a30d7d9300067443d5b3806030d3d6f3
Vincent Crame <vincentcrame7@gmail.com>
2026-03-13 16:49:59 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__ . '/settings.php';
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
