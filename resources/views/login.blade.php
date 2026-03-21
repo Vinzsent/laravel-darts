5240e7bcb56ddf5b751fddcf38b4bcafb052835b
Vincent Crame <vincentcrame7@gmail.com>
2026-03-21 11:40:53 +0800

    add: rout login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,18 +5,19 @@
 @section('content')
     <div class="container">
         <h1>Login Page</h1>
-        <form style="width: 50%; margin: 0 auto;" action="">
+        <form style="width: 50%; margin: 0 auto;" action="{{ route('login') }}" method="POST">
+            @csrf
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" name="remember">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
